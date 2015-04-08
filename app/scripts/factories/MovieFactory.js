b523ec2a453871e36b4e459275309a4415e6aae1
Daniel Tyson <danielctyson@gmail.com>
2015-04-08 14:54:09 -0400

    Fixed promises orders in movie details

--- app/scripts/factories/MovieFactory.js
+++ app/scripts/factories/MovieFactory.js
@@ -23,11 +23,9 @@ angular
                 return $http.post(ServerUrl + '/search', {guidebox_id: guideboxId, media_type: 'Movie'}).success(function(response){
                     angular.copy({}, movie);
                     angular.copy(response.movie, movie);
-                    movie.purchase_web_sources = JSON.parse(movie.purchase_web_sources);
-                    movie.subscription_web_sources = JSON.parse(movie.subscription_web_sources);
-                    movie.free_web_sources = JSON.parse(movie.free_web_sources);
-                    if (movie.rt_ratings) {movie.rt_ratings = JSON.parse(movie.rt_ratings);}
-                    if (movie.rt_reviews) {movie.rt_reviews = JSON.parse(movie.rt_reviews);}
+
+                    _parseJSON();
+
                     $('.ajax-preloader').removeClass('submitted');
 
                 }).error(function(data,status,headers,config){
@@ -37,8 +35,8 @@ angular
 
             var getMovieRatings = function(rottentomatoes_id) {
                 return $http.post(ServerUrl + '/search', {rottentomatoes_id: rottentomatoes_id, search_type: 'rt_ratings'}).success(function(response){
-                    angular.copy(response.movie, movie);
-                    if (movie.rt_ratings) {movie.rt_ratings = JSON.parse(movie.rt_ratings);}
+                    angular.copy(response.movie.rt_ratings, movie.rt_ratings);
+
                 }).error(function(data,status,headers,config){
                     console.log('Youre doing it wrong ' + data, status, headers, config);
                 });
@@ -47,8 +45,8 @@ angular
 
             var getMovieReviews = function(rottentomatoes_id) {
                 return $http.post(ServerUrl + '/search', {rottentomatoes_id: rottentomatoes_id, search_type: 'rt_reviews'}).success(function(response){
-                    angular.copy(response.movie, movie);
-                    if (movie.rt_reviews) {movie.rt_reviews = JSON.parse(movie.rt_reviews);}
+                    angular.copy(response.movie.reviews, movie.reviews);
+
                 }).error(function(data,status,headers,config){
                     console.log('Youre doing it wrong ' + data, status, headers, config);
                 });
@@ -58,9 +56,16 @@ angular
                 return $http.get('http://netflixroulette.net/api/api.php?title=' + movie.title).success(function(response){
                     movie.netflixLink = 'http://www.netflix.com/WiMovie/' + response.show_id;
                 }).error(function(data,status,headers,config){
-                    console.log(data.message);
                     movie.netflixLink = 'http://dvd.netflix.com/Search?v1=' + movie.title + '&ac_abs_posn=-1&fcld=true&ac_rel_posn=-1&ac_category_type=none';
                 });
+            };
+
+            var _parseJSON = function() {
+                if (movie.rt_ratings) {movie.rt_ratings = JSON.parse(movie.rt_ratings);}
+                if (movie.rt_reviews) {movie.rt_reviews = JSON.parse(movie.rt_reviews);}
+                movie.purchase_web_sources = JSON.parse(movie.purchase_web_sources);
+                movie.subscription_web_sources = JSON.parse(movie.subscription_web_sources);
+                movie.free_web_sources = JSON.parse(movie.free_web_sources);
             };
 
             return {
