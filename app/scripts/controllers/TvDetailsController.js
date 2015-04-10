257a0674e221275098f83ed99a5727d56f736b01
Daniel Tyson <danielctyson@gmail.com>
2015-04-10 00:40:11 -0400

    Gets episodes, but is crashy.  Will change to grab single episodes

--- app/scripts/controllers/TvDetailsController.js
+++ app/scripts/controllers/TvDetailsController.js
@@ -29,53 +29,13 @@ function tvDetailsController(TvFactory, $location, $routeParams, $window, Playli
         }
     };
 
-    // vm.hasStreamingLinks = function(){
-    //     if (!vm.movie.title || vm.movie.subscription_web_sources.length === 0 && !vm.movie.netflixLink) {
-    //         return false;
-    //     } else {
-    //         return true;
-    //     }
-    // };
+    vm.getEpisodes = function(){
+        if (!vm.tvShow.episodes) {
+            TvFactory.getEpisodes(vm.tvShow.guidebox_id);
+        };
+    };
 
-    // vm.hasPurchaseLinks = function(){
-    //     if (!vm.movie.title || vm.movie.purchase_web_sources.length === 0) {
-    //         return false;
-    //     } else {
-    //         return true;
-    //     }
-
-    // };
-
-    // vm.hasFreeLinks = function(){
-    //     if (!vm.movie.title || vm.movie.free_web_sources.length === 0) {
-    //         return false;
-    //     } else {
-    //         return true;
-    //     }
-    // };
-
-
-    // vm.getMovieRatings = function(){
-    //     if (!vm.movie.rt_ratings) {
-    //         TvFactory.getMovieRatings(vm.movie.rottentomatoes_id);
-    //     };
-    // };
-
-    // vm.getMovieReviews = function(){
-
-    //     if (!vm.movie.rt_reviews) {
-    //         TvFactory.getMovieReviews(vm.movie.rottentomatoes_id);
-    //     };
-    // };
-
-    // vm.getNetflixLink = function() {
-    //     TvFactory.getNetflixLink(vm.movie);
-    // };
-
-
-    TvFactory.getTvShowDetails($routeParams.guideboxId);
-
-    // .then(vm.getMovieRatings).then(vm.getMovieReviews).then(vm.getNetflixLink);
+    TvFactory.getTvShowDetails($routeParams.guideboxId).then(vm.getEpisodes);
 
     $('.main-content').on('click', function(){
         if ($('#navbar-collapse-1').hasClass('in')) {
--- app/scripts/factories/TvFactory.js
+++ app/scripts/factories/TvFactory.js
@@ -20,52 +20,28 @@ angular
                 angular.copy({}, tvShow);
                 return $http.post(ServerUrl + '/search', {guidebox_id: guideboxId, media_type: 'TV'}).success(function(response){
                     angular.copy(response.results, tvShow);
-                    // _parseJSON();
+                    tvShow.netflixLink = 'http://dvd.netflix.com/Search?v1=' + tvShow.title + '&ac_abs_posn=-1&fcld=true&ac_rel_posn=-1&ac_category_type=none';
+                    _parseJSON();
                 }).error(function(data,status,headers,config){
                     console.log('Youre doing it wrong ' + data, status, headers, config);
                 });
             };
 
-            // var getMovieRatings = function(rottentomatoes_id) {
-            //     return $http.post(ServerUrl + '/search', {rottentomatoes_id: rottentomatoes_id, search_type: 'rt_ratings'}).success(function(response){
-            //         movie.rt_ratings = JSON.parse(response.movie.rt_ratings);
-            //     }).error(function(data,status,headers,config){
-            //         console.log('Youre doing it wrong ' + data, status, headers, config);
-            //     });
-            // };
+            var getEpisodes = function(guideboxId) {
+                // return $http.post(ServerUrl + '/search', {guidebox_id: guideboxId, search_type: 'episodes'}).success(function(response){
+                //     tvShow.episodes = JSON.parse(response.tvShow.episodes);
+                // }).error(function(data,status,headers,config){
+                //     console.log('Youre doing it wrong ' + data, status, headers, config);
+                // });
+            };
 
-
-            // var getMovieReviews = function(rottentomatoes_id) {
-            //     return $http.post(ServerUrl + '/search', {rottentomatoes_id: rottentomatoes_id, search_type: 'rt_reviews'}).success(function(response){
-            //         movie.rt_reviews = JSON.parse(response.movie.rt_reviews);
-            //     }).error(function(data,status,headers,config){
-            //         console.log('Youre doing it wrong ' + data, status, headers, config);
-            //     });
-            // };
-
-            // var getNetflixLink = function(movie) {
-            //     return $http.get('http://netflixroulette.net/api/api.php?title=' + movie.title).success(function(response){
-            //         movie.netflixLink = 'http://www.netflix.com/WiMovie/' + response.show_id;
-            //     }).error(function(data,status,headers,config){
-            //         movie.netflixLink = 'http://dvd.netflix.com/Search?v1=' + movie.title + '&ac_abs_posn=-1&fcld=true&ac_rel_posn=-1&ac_category_type=none';
-            //     });
-            // };
-
-            // var _parseJSON = function() {
-            //     if (movie.rt_ratings) {movie.rt_ratings = JSON.parse(movie.rt_ratings);}
-            //     if (movie.rt_reviews) {movie.rt_reviews = JSON.parse(movie.rt_reviews);}
-            //     movie.purchase_web_sources = JSON.parse(movie.purchase_web_sources);
-            //     movie.subscription_web_sources = JSON.parse(movie.subscription_web_sources);
-            //     movie.free_web_sources = JSON.parse(movie.free_web_sources);
-            // };
+            var _parseJSON = function() {
+                if (tvShow.epsiodes) {tvShow.epsiodes = JSON.parse(tvShow.episodes);}
+            };
 
             return {
-                // movies: movies,
                 tvShow: tvShow,
-                // getMovies: getMovies,
+                getEpisodes: getEpisodes,
                 getTvShowDetails: getTvShowDetails,
-                // getMovieRatings: getMovieRatings,
-                // getMovieReviews: getMovieReviews,
-                // getNetflixLink: getNetflixLink
             };
     }]);
